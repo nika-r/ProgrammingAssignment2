a5be0f9e429dc85ebebdca67997c0ce574b09e30
Nika <r.veronika@gmail.com>
2014-06-21 23:08:28 +0100

    Programming Assignment Week2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+
+
+## makeCacheMatrix is a function that returns an object created from input argument (matrix).
+## The object also has another corresponding object (NULL by default) and functions which give full read/write access 
+## to both input argument (matrix) and corresponding object.
+## When argument object is changed corresponding object is set to NULL.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        invCM <- NULL
+        setCM <- function(y) {
+                x <<- y
+                invCM <<- NULL
+        }
+        getCM <- function() x
+        setIM <- function(matrx) invCM  <<- matrx
+        getIM <- function() invCM 
+        list(set = setCM , get = getCM ,
+             setIM = setIM ,
+             getIM = getIM)
 }
 
 
-## Write a short comment describing this function
+
+## cacheSolve function will need an object created by  makeCacheMatrix() function as an argument.
+## To call makeCacheMatrix(M) you need an argument M to be a square matrix
+## When called for the first time for certain argument it will calculate and return inverse matrix of the M and cache it 
+## All further calles for the same object will result in return of inverse matrix that was cached earlier.
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getIM()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setIM(m)
+        m
 }
